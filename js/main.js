655183ce7e5ac5a134555bbd89de43852799b9d9
Michael <michael.v.wenzel@ndus.edu>
2023-02-28 00:55:33 -0600

    Updated mobile layout.
    Added a call to action button. Made title and tagline easier to read by adding a darker background to the text.
    
    Added a "features" section.
    
    Added cards to feature kids and adult classes.
    
    Added microinteractions to call to action button, and "feature" section cards.

--- js/main.js
+++ js/main.js
@@ -9,3 +9,24 @@ menu.addEventListener('click', () => {
 close.addEventListener('click', () => {
     nav.classList.remove('open-nav');
 });
+
+var tl = new TimelineMax();
+const controller = new ScrollMagic.Controller();
+
+tl.from('#feature-img-1', 0.5, {x:200, opacity: 0, ease: Power4.easeInOut}, "=-1");
+
+const scene = new ScrollMagic.Scene({
+    triggerElement: ".more-info",
+    triggerHook: "onLeave",
+    duration: "100%"
+})
+  .setPin(".more-info")
+  .setTween(tl)
+    .addTo(controller);
+
+
+// Calculates percentage of a timeline
+function updatePercentage() {
+    tl.progress();
+    console.log(tl.progress());
+}
